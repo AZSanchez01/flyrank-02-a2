c27e1af504b284b2715e310f51ca525e6d59bf27
aivan <109073612+AZSanchez01@users.noreply.github.com>
2026-07-17 13:49:45 +0800

    feat: Add root and health endpoints

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.json({"name": "Task API", "version": "1.0", "endpoints": "/tasks"})
+})
+app.get("/health", (req, res) => {
+    res.json({"status": "ok"})
 })
 
 PORT = process.env.PORT || 3000
